a02faddc1c26357cff9b0814eed5e12927c8e74c
mprachaya <95976835+mprachaya@users.noreply.github.com>
2023-08-28 13:12:36 +0700

    display pdf in webview

--- _test/QuotationExportPDF.jsx
+++ _test/QuotationExportPDF.jsx
@@ -1,11 +1,27 @@
-import { Center, Text, View } from 'native-base';
+import { Center, ScrollView, Text, View } from 'native-base';
 import React from 'react';
+import WebView from 'react-native-webview';
 
 function QuotationExportPDF() {
+  const docType = 'Quotation';
+  const name = 'SAL-QTN-2023-00001';
+  const format = 'test-qt';
+  const key = '5891d01ccc2961e:0e446b332dc22aa';
+  const pdfUrl = `https://tonen.vsiam.com/api/method/frappe.utils.print_format.download_pdf?doctype=${docType}&name=${name}&format=${format}&key=${key}`;
   return (
     <View flex={1}>
       <Center m={24}>
         <Text>Quotation Export PDF</Text>
+        <View
+          mt={12}
+          w={'full'}
+          h={'full'}
+        >
+          <WebView
+            source={{ uri: pdfUrl }}
+            onLoad={() => console.log('PDF loaded')}
+          />
+        </View>
       </Center>
     </View>
   );
