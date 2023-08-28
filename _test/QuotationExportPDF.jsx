9bfb6aebf838cfe7b3d144ca1a1c8a8655d5c722
mprachaya <95976835+mprachaya@users.noreply.github.com>
2023-08-28 17:10:45 +0700

    print quotation work on ios not in android yet

--- _test/QuotationExportPDF.jsx
+++ _test/QuotationExportPDF.jsx
@@ -1,9 +1,9 @@
-import { Button, Center, Text, View } from 'native-base';
+import { Button, View } from 'native-base';
 import React, { useEffect, useState } from 'react';
-import { WebView } from 'react-native-webview';
+import Pdf from 'rn-pdf-reader-js';
 
 function QuotationExportPDF() {
-  const [pdfUri, setPdfUri] = useState(null);
+  const [pdfDataUri, setPdfDataUri] = useState('');
 
   async function printQuotation() {
     try {
@@ -17,75 +17,48 @@ function QuotationExportPDF() {
       const response = await fetch(apiUrl, {
         method: 'GET',
         headers: {
-          Authorization: 'Bearer 5891d01ccc2961e:0e446b332dc22aa',
+          Authorization: 'token 5891d01ccc2961e:0e446b332dc22aa',
         },
       });
 
-      // Handle the response here
       if (response.ok) {
         const pdfBlob = await response.blob();
-
-        // Convert the response blob to a Base64 data URL
         const reader = new FileReader();
         reader.readAsDataURL(pdfBlob);
         reader.onloadend = () => {
-          const pdfDataUrl = reader.result;
-          setPdfUri(pdfDataUrl);
-          console.log(pdfDataUrl);
+          let pdfBase64 = reader.result;
+          setPdfDataUri(pdfBase64);
         };
       } else {
-        // Handle errors if the request was not successful
         console.error('Error printing quotation:', response.status);
       }
     } catch (error) {
-      // Handle network errors or other issues
       console.error('Network error:', error);
     }
   }
+
   useEffect(() => {
     printQuotation();
   }, []);
 
-  // const docType = 'Quotation';
-  // const name = 'SAL-QTN-2023-00001';
-  // const format = 'test-qt';
-  // const key = '5891d01ccc2961e:0e446b332dc22aa';
-  // const pdfUrl = `https://tonen.vsiam.com/api/method/frappe.utils.print_format.download_pdf?doctype=${docType}&name=${name}&format=${format}&key=${key}`;
-
-  // const [showPdf, setShowPdf] = useState(false);
-
-  // const handleDownloadPDF = () => {
-  //   setShowPdf(true);
-  // };
-
   return (
     <View flex={1}>
-      {/* <Center mt={24}>
-        <Text>Quotation Export PDF</Text>
-        <View
-          mt={6}
-          w={'full'}
-          h={'full'}
-        >
-          {showPdf ? (
-            <WebView
-              source={{ uri: pdfUrl }}
-              style={{ flex: 1 }}
-              javaScriptEnabled={true}
-              onError={(error) => console.error('WebView Errror:', error)}
-              onLoad={() => console.log('WebView Loaded')}
-            />
-          ) : (
-            <Button
-              mx={6}
-              onPress={handleDownloadPDF}
-            >
-              Download PDF
-            </Button>
-          )}
-        </View>
-      </Center> */}
-      <Pdf source={pdfUri} />
+      {pdfDataUri !== '' && (
+        <Pdf
+          source={{ base64: pdfDataUri }} // Use the base64 property
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+            console.log(`File path: ${filePath}`);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`Current page: ${page}`);
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onError={(error) => {
+            console.error('Error:', error);
+          }}
+        />
+      )}
     </View>
   );
 }
